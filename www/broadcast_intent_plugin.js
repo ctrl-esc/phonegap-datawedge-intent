36963755a231e8c30a22c541fec727ff97e6639b
ctrl-esc <max@lsit.co>
2018-06-05 12:38:59 +0100

    Update broadcast_intent_plugin.js

--- www/broadcast_intent_plugin.js
+++ www/broadcast_intent_plugin.js
@@ -2,13 +2,9 @@ var cordova = require('cordova');
 var exec = require('cordova/exec');
 
 var BroadcastIntentPlugin = function() {
-
-  this.scan = function(success_cb, error_cb){
-    exec(success_cb, error_cb, "BarcodeScannerPlugin", "scan", []);
-  };
-
+  
   this.startListen = function(success_cb, error_cb){
-    exec(success_cb, error_cb, "BarcodeScannerPlugin", "start", []);
+    exec(success_cb, error_cb, "BroadcastIntentPlugin", "start", []);
   };
 
 };
